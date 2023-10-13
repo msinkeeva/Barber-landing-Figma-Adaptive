7421af6a5198eee3f1c401fb667f3f2f30c1f60d
msinkeeva <msinkeeva@bk.ru>
2023-10-13 15:12:38 +0600

    btn

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const menuBtn = document.querySelector(".menu__btn")
 const menuMobile = document.querySelector(".header__menu-list")
+const hideBtn = document.querySelector(".hide-img")
 
 menuBtn.addEventListener("click", () => {
+  menuMobile.classList.toggle("menu__open")
+})
+
+hideBtn.addEventListener("click", () => {
   menuMobile.classList.toggle("menu__open")
 })
